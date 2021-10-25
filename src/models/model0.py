d84b21b2c17557901caf4566c73f252dcb18575f
dimiphoton <pro.dimitrimarchand@gmail.com>
2021-10-25 09:58:45 +0200

    notebook

---
+++ src/models/model0.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+"""
+fonction qui renvoie la nouvelle température moyenne
+arguments:
+
+"""
+def update(set):
+    return set
+
+
---
+++ src/models/train_model.py
@@ -0,0 +1,5 @@
+#import model0
+
+if __name__ == '__main__':
+    #print(model0.update(0))
+    print('hello')
